41aee61470e4b50be4103ebf4b3fe95fc6b5e285
Ibrahim Yusuf <iy830755@gmail.com>
2023-08-30 16:38:02 +0100

    add production stuff

--- db/connection.js
+++ db/connection.js
@@ -5,11 +5,13 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool({
-  username: process.env.PG_USER,
-  password: process.env.PG_USER_PASSWORD
-});
+const config = {};
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+module.exports = new Pool(config);
